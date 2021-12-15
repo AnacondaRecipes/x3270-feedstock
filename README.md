58bdb1debb47d819d7a3b0a3578a6c1e64a83ee0
Cheng H. Lee <clee@anaconda.com>
2021-12-15 17:45:50 -0600

    Initial feedstock commit with conda-smithy 3.15.1.

---
+++ README.md
@@ -0,0 +1,21 @@
+About x3270
+=================
+
+Home: http://x3270.bgp.nu/
+
+Package license: BSD-3-Clause
+
+Feedstock license: [BSD-3-Clause](https://github.com/conda-forge/x3270-split-feedstock/blob/master/LICENSE.txt)
+
+Summary: Suite of IBM 3270 terminal emulators
+
+Development: https://sourceforge.net/projects/x3270/
+
+Documentation: https://x3270.miraheze.org/wiki/Main_Page
+
+
+Feedstock Maintainers
+=====================
+
+* [@chenghlee](https://github.com/chenghlee/)
+
---
+++ recipe/build.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+declare -a config_opts
+
+config_opts+=(--enable-option-checking)
+
+## What to build
+config_opts+=(--enable-lib)         # Unix support libraries
+config_opts+=(--enable-pr3287)      # 3287 line printer emulator
+
+#config_opts+=(--enable-b3270)       # generic 3270 emulator backend
+config_opts+=(--enable-c3270)       # character-mode 3270 emulator
+#config_opts+=(--enable-x3270)       # X11 3270 emulator
+
+config_opts+=(--enable-s3270)       # 3270 screen scraping tool
+config_opts+=(--enable-tcl3270)     # Scripting support for s3270
+
+## Enable TLS support
+config_opts+=(--enable-tls)
+config_opts+=(--with-openssl="${PREFIX}")
+config_opts+=(--with-ssl="${PREFIX}")
+#config_opts+=(--enable-mock-tls)        # TLS mock for testing
+
+## Other common features
+config_opts+=(--enable-ipv6)
+config_opts+=(--enable-dbcs)
+config_opts+=(--enable-local-process)
+
+config_opts+=(--with-iconv)     # Use iconv instead of `__STDC_ISO_10646__`
+config_opts+=(--with-readline)
+
+#config_opts+=(--with-x)
+#config_opts+=(--with-fontdir=DIR)       # install directory for X11 fonts
+#config_opts+=(--enable-app-defaults)    # separate app-defaults file
+
+case "$target_platform" in
+    osx-*)
+        # Use conda-provided OpenSSL for TLS support
+        config_opts+=(--disable-stransport)
+        ;;
+esac
+
+./configure --prefix="${PREFIX}" ${config_opts[@]}
+make -j${CPU_COUNT}
+make unix-install
